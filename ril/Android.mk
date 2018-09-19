222dbe016594d4696c5faebd44ce4b1dff39ad7a
Kicky Vivi <vivi94458@gmail.com>
2018-09-19 21:46:07 +0530

    adding libril_shim 2

--- ril/Android.mk
+++ ril/Android.mk
@@ -14,4 +14,11 @@
 # limitations under the License.
 #
 
-include $(call first-makefiles-under,$(call my-dir))
+LOCAL_PATH := $(call my-dir)
+ include $(CLEAR_VARS)
+ LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
+ LOCAL_SRC_FILES := \
+    ril_shim.c
+ LOCAL_MODULE := libril_shim
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+ include $(BUILD_SHARED_LIBRARY)
